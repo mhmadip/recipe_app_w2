89e096ebce0595c4e6b2f5314a40c2bb6aa0da27
Mohammad Salim <mhmadip@gmail.com>
2022-08-28 15:29:07 +0300

    final update to the codes

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -1,30 +1,18 @@
 class Recipe {
   String label;
   String url;
+  int servings;
+  List<Ingredient> ingrs;
 
-  Recipe(
-    this.label,
-    this.url,
-  );
+  Recipe(this.label, this.url, this.servings, this.ingrs);
   // Todo: Add Recipe List
 
   static List<Recipe> samples = [
-    Recipe(
-      'Spagheti',
-      '1.jpeg',
-    ),
-    Recipe(
-      'Spagheti',
-      '1.jpeg',
-    ),
-    Recipe(
-      'Spagheti',
-      '1.jpeg',
-    ),
-    Recipe(
-      'Spagheti',
-      '1.jpeg',
-    ),
+    Recipe('Spagheti', '1.jpeg', 1,
+        [Ingredient(2, 'can', 'sauce'), Ingredient(2, 'can', 'sauce')]),
+    Recipe('Spagheti', '1.jpeg', 1, [Ingredient(2, 'can', 'sauce')]),
+    Recipe('Spagheti', '1.jpeg', 1, [Ingredient(2, 'can', 'sauce')]),
+    Recipe('Spagheti', '1.jpeg', 1, [Ingredient(2, 'can', 'sauce')]),
   ];
 }
 
